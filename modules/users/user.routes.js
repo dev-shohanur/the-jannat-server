2453a442b6d18b540eab1908aba29b98c35b5154
MD Shohanur Rahman <shohan.ridigames@gmail.com>
2023-12-11 05:16:02 +0600

    Update

--- modules/users/user.routes.js
+++ modules/users/user.routes.js
@@ -3,18 +3,18 @@ const { loginUserMyApp, getCurentUser, getAllUser, getUserById, getAllTailor } =
 const userRoutes = express.Router();
 
 // Define the user routes
-// userRoutes.post("/login", (req, res) => {
-//   loginUserMyApp(req, res);
-// });
-// userRoutes.post("/curent-user", (req, res) => {
-//   getCurentUser(req, res);
-// });
+userRoutes.post("/login", (req, res) => {
+  loginUserMyApp(req, res);
+});
+userRoutes.post("/curent-user", (req, res) => {
+  getCurentUser(req, res);
+});
 
-// userRoutes.get("/", (req, res) => {
-//   getAllUser(req, res);
-// });
-// userRoutes.get("/:id", (req, res) => {
-//   getUserById(req, res);
-// });
+userRoutes.get("/", (req, res) => {
+  getAllUser(req, res);
+});
+userRoutes.get("/:id", (req, res) => {
+  getUserById(req, res);
+});
 
 module.exports = userRoutes;
